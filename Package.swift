63e894972be4bd38d384cf315d428232ff59322e
최종수 <chlwhdtn03@naver.com>
2026-05-31 10:52:19 +0900

    1.3.4 checksum

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "LmsApi",
             url: "https://github.com/chlwhdtn03/LMS-API/releases/download/1.3.4/LmsApi.xcframework.zip",
-            checksum: "97efd03b52648bd809bdf477b03431e575d7e3965ba3bc6241c9ac462baa9dc5"
+            checksum: "ee2ab1b6eb611c625b5759b67fea7b3e276c5fef2d7df85ff82a880c4472ceff"
         )
     ]
 )
