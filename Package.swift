5d8c286c7b03f48b62abd87d5a9a897fc5efa4f6
최종수 <chlwhdtn03@naver.com>
2026-06-19 16:52:56 +0900

    release 1.5.0

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LmsApi",
-            url: "https://github.com/chlwhdtn03/LMS-API/releases/download/1.4.5/LmsApi.xcframework.zip",
-            checksum: "9f47ef965c33b0d0d03d1ffd538771cc052daf2b17eac1de5cb24a511abff489"
+            url: "https://github.com/chlwhdtn03/LMS-API/releases/download/1.5.0/LmsApi.xcframework.zip",
+            checksum: "90e52158eed51e418321b6f4eca86990a01283f0a23435c0aa70e0ff58f44cc2"
         )
     ]
 )
